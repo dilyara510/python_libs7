c31f5924159ff182bd14c0499ae0adb973b93b60
Sayfullina Dilyara <dilyarasaufullina@yandex.ru>
2023-11-26 18:06:49 +0300

    database with admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Task,Topic,Message
+# , Topic, Message, User
+
+admin.site.register(Task)
+admin.site.register(Topic)
+admin.site.register(Message)
--- base/views.py
+++ base/views.py
@@ -1,23 +1,21 @@
 from django.shortcuts import render
+from .models import Task 
 
-
-tasks = [
-    {'id': 1, 'name': 'Задание 1'},
-    {'id': 2, 'name': 'Задание 2'},
-    {'id': 3, 'name': 'Задание 3'},
-]
+# tasks = [
+#     {'id': 1, 'name': 'Задание 1'},
+#     {'id': 2, 'name': 'Задание 2'},
+#     {'id': 3, 'name': 'Задание 3'},
+# ]
 
 
 def home(request):
+    tasks=Task.objects.all()
     context = {'tasks': tasks}
     return render(request, 'base/home.html', context)
 
 
 def task(request, pk):
-    task = None
-    for i in tasks:
-        if i['id'] == int(pk):
-            task = i
+    task = Task.objects.get(id=pk)
     context = {'task': task}
     return render(request, 'base/task.html', context)
 
