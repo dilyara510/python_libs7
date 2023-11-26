0618d23664aa9e93d94b67ebe961cfb2b498b708
Sayfullina Dilyara <dilyarasaufullina@yandex.ru>
2023-11-26 21:55:00 +0300

    create update delete

--- base/urls.py
+++ base/urls.py
@@ -10,4 +10,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('task/<str:pk>/', views.task, name="task"),
+
+    path('create-task/',views.createTask, name="create-task"),
+    path('update-task/<str:pk>/',views.updateTask, name="update-task"),
+    path('delete-task/<str:pk>/',views.deleteTask, name="delete-task"),
+
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task 
+from .forms import TaskForm
 
 # tasks = [
 #     {'id': 1, 'name': 'Задание 1'},
@@ -22,3 +23,36 @@ def task(request, pk):
 
 # def task(request,pk):
 #     return render(request,'base/task.html')
+
+def createTask(request):
+    form=TaskForm()
+    if request.method=='POST':
+        form=TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context={'form':form}
+    return render(request, 'base/task_form.html', context)
+
+
+def updateTask(request, pk):
+    task=Task.objects.get(id=pk)
+    form=TaskForm(instance=task)
+
+    if request.method=='POST':
+        form=TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        
+    context={'form':form}
+
+    return render(request, 'base/task_form.html', context)
+
+def deleteTask(request,pk):
+    task=Task.objects.get(id=pk)
+    if request.method=='POST':
+        task.delete()
+        return redirect('home')
+
+    return render(request, 'base/delete.html',{'obj':task} )
