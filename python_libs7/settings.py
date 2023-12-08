b86f2163898769506a77620ad946e794ffc5a48a
Danil Yusupov <danil.yusupov@simbirsoft.com>
2023-12-08 17:51:37 +0300

    changed allowed_hosts

--- python_libs7/settings.py
+++ python_libs7/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-j8)n*p$zb1(z!fik(6de%%r1di^j5!5##!%l0w^h1jqj1)-@!z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
